939d82a39ed9e0b1e67c6281eba1da788c5cd7b5
eliudg4 <juniorgzn24@gmail.com>
2022-02-08 18:46:54 -0500

    reservation model is set up

---
+++ models/reservation.js
@@ -0,0 +1,15 @@
+const mongoose = require('../db/connection')
+
+const reservationSchema = new mongoose.Schema({
+    firstName: {type: String, required: true},
+    lastName: {type: String, required: true},
+    email: {type: String, required: true},
+    date: {type: Date, default: Date.now},
+    time: {type: String, required: true},
+    partySize: {type: Number, default: 1, required: true},
+    table: {type: Number, required: true}
+})
+
+const Reservation = mongoose.model('Reservation', reservationSchema)
+
+module.exports = Reservation
