aa784c2ec9620932b93426e56815a06a43a53702
ispoon21 <isaiahweathersp@gmail.com>
2022-02-20 17:29:40 -0700

    edit and delete routes added to cart controller. Email confirmation also configed

--- controllers/cart.js
+++ controllers/cart.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const Cart = require('../models/cart')
+// email confirmation setup
+const nodemailer = require('nodemailer');
 
 // Index Route
 router.get('/cart', (req, res) => {
@@ -23,7 +25,38 @@ router.post('/cart', async(req, res)=> {
 
             //create order once the form has been submitted
             res.status(200).json(createdOrder);
-            console.log(createdOrder)
+
+
+
+             // send confirmation email to user once order form is submitted by user.
+                let transporter = nodemailer.createTransport({
+                    service: "gmail",
+                    port: 587,
+                    secure: false,
+                    auth: {
+                        user: "teamfiveconfirmation@gmail.com", 
+                        pass: process.env.EMAIL_INFO, 
+                    },
+                    // ensure we can test email confirmation from local host
+                    tls:{
+                        rejectUnauthorized: false
+                    }
+                });
+
+            //  user email details section
+        
+                let info =  transporter.sendMail({
+                    from: '"TeamFive Team" <teamfiveconfirmation@gmail.com>', // sender address
+                    to: `${req.body.email}`, // list of receivers
+                    subject: "Reservation Confirmation ✅", // Subject line
+                    text: "Hello", // plain text body
+                    html: `<b>Hello ${req.body.firstName}</b> <br> <p>Your food order has been recieved and will be ready shortly!</b><br> Please <a href="http://localhost:4000/menu/cart/${createdOrder._id}">click here</a> to change or cancel your order. This email will be your only record of your order. 
+                    </p><br> Thank you,<br> <b>TeamFive Eatery</b>`, // html body
+                });
+
+                console.log("Message sent: %s", info.messageId);
+                // Preview only available when sending through an Ethereal account
+                console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
                 
         }
     })
@@ -39,6 +72,28 @@ router.get('/cart/:id', (req, res) => {
         } else {
                 res.status(200).json(cart)
             
+        }
+    })
+})
+
+// delete route
+router.delete('/cart/:id', (req, res)=> {
+    Cart.findByIdAndDelete(req.params.id, (error, order) => {
+        if(error){
+            res.status(400).json({error: error.message})
+        } else {
+            res.status(200).json(order)
+        }
+    })
+})
+
+// update route 
+router.put('/cart/edit/:id', (req, res)=> {
+    Cart.findByIdAndUpdate(req.params.id, req.body, {new: true}, (error, updateOrder)=> {
+        if(error){
+            res.status(400).send({error: error.message})
+        } else {
+            res.status(200).send(updateOrder);
         }
     })
 })
