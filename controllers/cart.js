cffa0ee19cb69b4df98317ba067824582eeae112
john-330 <boss_jaxon@mail.ru>
2022-02-21 20:27:44 -0600

    .env file was deleted and cart update route was modified

--- controllers/cart.js
+++ controllers/cart.js
@@ -29,35 +29,35 @@ router.post('/cart', async(req, res)=> {
 
 
 
-             // send confirmation email to user once order form is submitted by user.
-                let transporter = nodemailer.createTransport({
-                    service: "gmail",
-                    port: 587,
-                    secure: false,
-                    auth: {
-                        user: "teamfiveconfirmation@gmail.com", 
-                        pass: process.env.EMAIL_INFO, 
-                    },
-                    // ensure we can test email confirmation from local host
-                    tls:{
-                        rejectUnauthorized: false
-                    }
-                });
+            //  // send confirmation email to user once order form is submitted by user.
+            //     let transporter = nodemailer.createTransport({
+            //         service: "gmail",
+            //         port: 587,
+            //         secure: false,
+            //         auth: {
+            //             user: "teamfiveconfirmation@gmail.com", 
+            //             pass: process.env.EMAIL_INFO, 
+            //         },
+            //         // ensure we can test email confirmation from local host
+            //         tls:{
+            //             rejectUnauthorized: false
+            //         }
+            //     });
 
             //  user email details section
         
-                let info =  transporter.sendMail({
-                    from: '"TeamFive Team" <teamfiveconfirmation@gmail.com>', // sender address
-                    to: `${req.body.email}`, // list of receivers
-                    subject: "Reservation Confirmation ✅", // Subject line
-                    text: "Hello", // plain text body
-                    html: `<b>Hello ${req.body.firstName}</b> <br> <p>Your food order has been recieved and will be ready shortly!</b><br> Please <a href="http://localhost:4000/menu/cart/${createdOrder._id}">click here</a> to change or cancel your order. This email will be your only record of your order. 
-                    </p><br> Thank you,<br> <b>TeamFive Eatery</b>`, // html body
-                });
+                // let info =  transporter.sendMail({
+                //     from: '"TeamFive Team" <teamfiveconfirmation@gmail.com>', // sender address
+                //     to: `${req.body.email}`, // list of receivers
+                //     subject: "Reservation Confirmation ✅", // Subject line
+                //     text: "Hello", // plain text body
+                //     html: `<b>Hello ${req.body.firstName}</b> <br> <p>Your food order has been recieved and will be ready shortly!</b><br> Please <a href="http://localhost:4000/menu/cart/${createdOrder._id}">click here</a> to change or cancel your order. This email will be your only record of your order. 
+                //     </p><br> Thank you,<br> <b>TeamFive Eatery</b>`, // html body
+                // });
 
-                console.log("Message sent: %s", info.messageId);
-                // Preview only available when sending through an Ethereal account
-                console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+                // console.log("Message sent: %s", info.messageId);
+                // // Preview only available when sending through an Ethereal account
+                // console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
                 
         }
     })
@@ -89,8 +89,9 @@ router.delete('/cart/:id', (req, res) => {
 })
 
 //update route
-router.put('/cart/:id', (req, res) => {
+router.put('/cart/edit/:id', (req, res) => {
     Cart.findByIdAndUpdate(req.params.id, req.body, {new: true}, (error, cart) =>{
+        console.log(error);
         if(error) {
             res.status(400).json({error: error.message})
         } else {
@@ -99,29 +100,6 @@ router.put('/cart/:id', (req, res) => {
         }
     })
 })
-
-// delete route
-router.delete('/cart/:id', (req, res)=> {
-    Cart.findByIdAndDelete(req.params.id, (error, order) => {
-        if(error){
-            res.status(400).json({error: error.message})
-        } else {
-            res.status(200).json(order)
-        }
-    })
-})
-
-// update route 
-router.put('/cart/edit/:id', (req, res)=> {
-    Cart.findByIdAndUpdate(req.params.id, req.body, {new: true}, (error, updateOrder)=> {
-        if(error){
-            res.status(400).send({error: error.message})
-        } else {
-            res.status(200).send(updateOrder);
-        }
-    })
-})
-
 
 
 module.exports = router
